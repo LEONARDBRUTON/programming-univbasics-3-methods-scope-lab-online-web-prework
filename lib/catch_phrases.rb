78feb88694d98a97432ace23bf43952367848ed7
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-10-25 16:33:59 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,4 @@
-=begin def phrase
-describe "mario" do
-  it "puts out 'It's-a me, Mario!'" do
-    expect{mario}.to output("It's-a me, Mario!\n").to_stdout
-=end  end
+
 
   def mario
    phrase = 'It's-a me, Mario !'
@@ -13,6 +9,4 @@ describe "mario" do
     status = 'Thank You Mario! But Our Princess Is In Another Castle'
    puts status
   end
-#end
-#end
 
